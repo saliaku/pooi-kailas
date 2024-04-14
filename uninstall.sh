8ee2dbb9443650246bbd786f622d11f3a7c46edc
kailas <kailasnath727@gmail.com>
2024-04-15 03:01:06 +0530

    added some comments to uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+# Check if the system is Linux
 if [ $(uname) != 'Linux' ]; then
     echo "This uninstall script is designed for linux only, sorry!"
     exit 1
 fi
 
-set -e
+set -e  # Exit immediately if any command exits with a non-zero status
 
+# Define text formatting variables for colorful output
 RED='[1;31m'
 GREEN='[1;32m'
 YELLOW='[1;33m'
@@ -14,6 +16,7 @@ CYAN='[1;36m'
 BOLD='[1m'
 RESET='[0m'
 
+# Print a cool banner
 printf '\n%b' $CYAN && cat << 'EOF'
       ▪  ▄▄
 ▪     ██ ██▌
@@ -23,18 +26,20 @@ printf '\n%b' $CYAN && cat << 'EOF'
 EOF
 printf '%b' $RESET
 
+# Check if running as root
 if [ $(id -u) = 0 ]; then
     printf "%bwarning:%b please don't run random scripts you find on the internet as root!\n" $YELLOW $RESET
     printf '%bsudo or doas will be used when elevated privileges are required%b\n' $BOLD $RESET
     exit 1
 fi
 
-# check if pooi is not in path
+# Check if 'pooi' is in the system's PATH
 if ! command -v pooi >/dev/null ; then
     printf '%berror:%b can not find %boi%b in your $PATH, are you sure that it is installed?\n' $RED $RESET $BOLD $RESET
     exit 1
 fi
 
+# Prompt user for confirmation before proceeding with uninstallation
 printf "%balert!%b are you sure that you wish to remove pooi from your system? [Y/n] " $YELLOW $RESET
 while true; do
     read yn
@@ -45,6 +50,7 @@ while true; do
     esac
 done
 
+# Determine the available privilege escalation command (sudo or doas)
 if command -v sudo >/dev/null 2>&1; then
     PRIV_ESC='sudo'
 elif command -v doas >/dev/null 2>&1; then
@@ -54,14 +60,17 @@ else
     exit 1
 fi
 
+# Get the location of 'pooi' executable
 LOC=$(command -v pooi)
 
+# Remove files associated with 'pooi' using elevated privileges
 printf '\n%bremoving files%b (elevated privileges are required)\n\n' $GREEN $RESET
 $PRIV_ESC rm -v $LOC
 $PRIV_ESC rm -v /usr/share/zsh/site-functions/_oi
 $PRIV_ESC rm -v /usr/share/bash-completion/completions/pooi
 $PRIV_ESC rm -v /usr/share/fish/vendor_completions.d/pooi.fish
 
+# Check if the removal was successful
 if [ ! -f $LOC ]; then
     printf '\n%bDONE:%b done removing files!\n' $GREEN $RESET
 else
