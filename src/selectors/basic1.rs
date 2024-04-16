71d297dad5f83fc34d88527cee0c7350d195b16e
kailas <kailasnath727@gmail.com>
2024-04-16 07:05:33 +0530

    basic1 mod documented

--- src/selectors/basic1.rs
+++ src/selectors/basic1.rs
@@ -1,9 +1,17 @@
+// Import necessary functions from the selectors module
 use crate::selectors::{default_output, get_vec};
 
+// Define the main function that takes HTML data, a boolean flag for tty, and a width
 pub fn main(data: &scraper::Html, tty: &bool, w: usize) {
+    // Extract text from the HTML data based on the selector
     let x = get_vec(data, "div.HwtpBd.gsrt.PZPZlf.kTOYnf");
+    
+    // Check if the tty flag is true or false
     match tty {
+        // If tty is true, use the default_output function to display the result
         true => default_output(x[0], w),
+        
+        // If tty is false, simply print the result
         false => println!("{}", x[0])
     }
 }
